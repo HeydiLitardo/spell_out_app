99a220f77d8cc982bc31fce3ddc668d6e992034a
HeydiLitardo <heydi.litardo2018@uteq.edu.ec>
2023-02-16 22:55:27 -0500

    Pantalla de opciones profesor

--- app/src/main/java/com/example/spellout/Inicio.kt
+++ app/src/main/java/com/example/spellout/Inicio.kt
@@ -2,10 +2,50 @@ package com.example.spellout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import com.google.android.material.bottomappbar.BottomAppBar
 
 class Inicio : AppCompatActivity() {
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_inicio)
+
+        title = "Opciones del Profesor"
+
+        val alumnos = findViewById<ImageView>(R.id.alumnos)
+        val crearActividad = findViewById<ImageView>(R.id.crear_actividad)
+        val notas = findViewById<ImageView>(R.id.notas)
+        val asignarActividad = findViewById<ImageView>(R.id.asignar_actividad)
+        val btnRegistrar = findViewById<Button>(R.id.btn_registrar_alumnos)
+        val btnAtras = findViewById<Button>(R.id.btn_atras)
+
+        alumnos.setOnClickListener {
+
+        }
+
+        crearActividad.setOnClickListener {
+
+        }
+
+        notas.setOnClickListener {
+
+        }
+
+        asignarActividad.setOnClickListener {
+
+        }
+
+        btnRegistrar.setOnClickListener {
+
+        }
+
+        btnAtras.setOnClickListener {
+
+        }
     }
+
 }
